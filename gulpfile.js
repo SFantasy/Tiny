97705ef2af2aedbb390d84ee9391002cea19be4d
Fantasy Shao <forever.fantasy27@gmail.com>
2014-11-21 20:56:23 +0800

    Add gulp-rename for the project.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@
  * @description auto build JavaScript and Stylesheets for Tiny
  * @author Fantasy <fantasyshao@icloud.com>
  * @create 2014-11-14
- * @update 2014-11-15
+ * @update 2014-11-21
  */
 
 var gulp = require('gulp');
@@ -13,20 +13,21 @@ var gulp = require('gulp');
 var coffee = require('gulp-coffee'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
-    sass = require('gulp-ruby-sass');
+    sass = require('gulp-ruby-sass'),
+    rename = require('gulp-rename');
 
 var paths = {
   scripts: 'src/coffee/*.coffee',
   sass: 'src/sass/*.sass',
   js: 'dist/js/*.js',
-  css: 'dist/css/*.css'
+  tiny_sass: 'src/sass/tiny.sass'
 };
 
 gulp.task('default', ['scripts', 'sass']);
 
 gulp.task('scripts', function () {
   gulp.src(paths.scripts)
-    .pipe(coffee({bare: true}))
+    .pipe(coffee({ bare: true }))
     .pipe(gulp.dest('dist/js'));
 });
 
@@ -37,11 +38,27 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('build', ['scripts', 'sass'], function () {
+// Build files into production folder
+gulp.task('build', ['scripts'], function () {
   gulp.src(paths.js)
     .pipe(uglify())
     .pipe(concat('tiny.min.js'))
     .pipe(gulp.dest('production'));
+
+  gulp.src(paths.tiny_sass)
+    .pipe(sass({ style: 'compressed', noCache: true, 'sourcemap=none': true }))
+    .on('error', function (err) { console.log(err.message); })
+    .pipe(rename('tiny.min.css'))
+    .pipe(gulp.dest('production'));
+});
+
+// Example
+gulp.task('eg', ['build'], function () {
+  gulp.src('production/tiny.min.js')
+    .pipe(gulp.dest('examples/js'));
+
+  gulp.src('production/tiny.min.css')
+    .pipe(gulp.dest('examples/css'));
 });
 
 gulp.task('watch', function () {
